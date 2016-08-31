cdb935edbda789280b546a707c26e8bbf4c752af
Mario Celi <mceli@stackbuilders.com>
2016-08-31 09:40:58 -0500

    Add more specific number matching regex

--- src/main/java/ApiProxy.java
+++ src/main/java/ApiProxy.java
@@ -26,7 +26,7 @@ public class ApiProxy {
     String phoneNumberPattern =
         "\\/\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*";
     String escapedPhoneNumberPatter =
-        "\\/\\\\s*(?:((?:%2B)|(?:\\\\+))?(\\\\d{1,3}))?[-. (%20B]*(\\\\d{3})[-. (%20B]*(\\\\d{3})[-. %20B]*(\\\\d{4})(?: *x(\\\\d+))?";
+        "\\/\\\\s*(?:((?:%2B)|(?:\\\\+))?(\\\\d{1,3}))?(?:\\-|\\.| |\\(|(?:%20)|(?:%28)|(?:%2E)|(?:%2D))*(\\\\d{3})(?:\\-|\\.| |\\)|(?:%20)|(?:%29)|(?:%2E)|(?:%2D))*(\\\\d{3})(?:\\-|\\.| |(?:%20)|(?:%2E)|(?:%2D))*(\\\\d{4})";
 
     WireMockServer wireMockServer = new WireMockServer(wireMockConfig().port(8089).httpsPort(443)
         .keystorePath("keystore/twilio-store.jks").keystorePassword("twilioFake"));
